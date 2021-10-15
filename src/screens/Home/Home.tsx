e7a034b49c46a764ccb98a88c4e1ccd05433ee1c
LeoGonzaga <sacredlotus05@gmail.com>
2021-10-14 22:53:20 -0300

    fix remote repo

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,24 @@
-import React, { useCallback, useState } from 'react';
-import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native';
-import About from '../../components/About';
-import OrderModal from '../../components/OrderModal';
-import Quantity from '../../components/Quantity';
-import Title from '../../components/Title';
-import colors from '../../constants/colors';
+import React, { useCallback, useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+  interpolate,
+  Extrapolate,
+} from "react-native-reanimated";
 
-import Pizza from '../../../assets/pizza.png';
+import About from "../../components/About";
+import OrderModal from "../../components/OrderModal";
+import Quantity from "../../components/Quantity";
+import Title from "../../components/Title";
+import colors from "../../constants/colors";
+import Pizza from "../../../assets/pizza.png";
 
 export const Home = () => {
+  const sharedVal = useSharedValue(0);
   const [quantity, setQuantity] = useState<number>(1);
   const [visible, setVisible] = useState<boolean>(false);
 
@@ -24,9 +34,24 @@ export const Home = () => {
     setVisible(!visible);
   }, [visible]);
 
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotateZ: withSpring(`${sharedVal.value}deg`) }],
+      opacity: interpolate(sharedVal.value, [100, 0], [0, 1], Extrapolate.CLAMP),
+    };
+  });
+
+  useEffect(() => {
+    sharedVal.value = 100;
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Image source={Pizza} style={styles.image} resizeMode="contain" />
+      <Animated.Image
+        source={Pizza}
+        style={[styles.image, animatedStyles]}
+        resizeMode="contain"
+      />
       <Title />
       <About />
 
@@ -52,32 +77,35 @@ export const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
   },
   image: {
-    height: '50%',
-    width: '100%',
+    height: "50%",
+    width: "90%",
+    margin: 0,
+    padding: 0,
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   primaryButton: {
     width: 150,
     height: 60,
     backgroundColor: colors.details,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 15,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 20,
   },
   wrapper: {
-    justifyContent: 'space-around',
-    width: '100%',
+    justifyContent: "space-around",
+    width: "100%",
   },
 });
